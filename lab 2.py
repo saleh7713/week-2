62c3e00e4c36daab92f86455292578cd38e473a7
Saleh Almarri (s5724906) <s5724906@bournemouth.ac.uk>
2024-10-15 12:44:39 +0100

    Initial message

---
+++ lab 2.py
@@ -0,0 +1,21 @@
+x = ("Look! I will become the best Python programme")
+y = 10
+z = 5.5
+print(type(x))
+print(type(y))
+print(type(z))
+print(y+z)
+print(y+int(z))
+print(z+float(y))
+print(str(z))
+print(x + str(y) + str(z))
+my_text= ("Hello, World")
+print (my_text.upper())
+my_text2="I have an apple, you have an apple."
+print(my_text2.replace("apple" ,"orange"))
+my_text3="    I have an apple, you have an apple.    "
+print(my_text3.strip())
+my_text4="Python Java C++"
+print(my_text4.split())
+my_text5="Bananas are great source of Potassium"
+print(my_text5.count("a"))
